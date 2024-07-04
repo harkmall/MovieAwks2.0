961b2cbb51c18e074bdf058c9eeb2659e360a592
Mark Hall <golfguru@icloud.com>
2024-07-04 14:29:45 -0400

    Fixing auth

--- MovieAwks-Backend/Sources/App/routes.swift
+++ MovieAwks-Backend/Sources/App/routes.swift
@@ -5,7 +5,7 @@ func routes(_ app: Application) throws {
     let unprotectedAPI = app.grouped("api")
     try unprotectedAPI.grouped("auth", "siwa").register(collection: SIWAController())
     
-    let tokenProtectedAPI = unprotectedAPI.grouped(Token.authenticator())
+    let tokenProtectedAPI = unprotectedAPI.grouped(Token.authenticator()).grouped(User.guardMiddleware())
     try tokenProtectedAPI.grouped("users").register(collection: UserController())
     try tokenProtectedAPI.register(collection: TMDBController())
 }
